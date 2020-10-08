fda5593aadfa3d5a0387959eb8de6aa79a48bda6
Yossi Cohen <metalyos@gmail.com>
2020-10-09 01:02:48 +0300

    Added EventService class which implements Pub Sub design pattern.
    Simple implementation used to communicate between widgets.

---
+++ eventsservice.cpp
@@ -0,0 +1,37 @@
+#include "eventsservice.h"
+
+int EventsService::_subId = 0;
+
+EventsService::EventsService()
+{
+
+}
+
+int EventsService::Subscribe(EventsTypes type, EventCallback callback)
+{
+    if (type == EventsTypes::None)
+        return -1;
+
+    _callbacks[(int)type][_subId] = callback;
+    return _subId++;
+}
+
+void EventsService::UnSubscribe(EventsTypes type, int id)
+{
+    if (type == EventsTypes::None)
+        return;
+
+    _callbacks[(int)type].erase(id);
+}
+
+void EventsService::Publish(EventsTypes type, void* data)
+{
+    if (type == EventsTypes::None)
+        return;
+
+    auto pairs = _callbacks[(int)type];
+    for (auto pair : pairs)
+    {
+        pair.second(data);
+    }
+}
---
+++ eventsservice.h
@@ -0,0 +1,37 @@
+#ifndef EVENTSSERVICE_H
+#define EVENTSSERVICE_H
+
+#include <map>
+#include <functional>
+
+typedef std::function<void(void*)> EventCallback;
+
+enum EventsTypes
+{
+    StartBgColorPick,
+    EndBgColorPick,
+    None
+};
+
+class EventsService
+{
+public:
+    static EventsService& Instance()
+    {
+        static EventsService instance;
+        return instance;
+    }
+
+    int Subscribe(EventsTypes type, EventCallback callback);
+    void UnSubscribe(EventsTypes type, int id);
+    void Publish(EventsTypes type, void* data);
+
+private:
+    EventsService();
+
+private:
+    static int _subId;
+    std::map<int, EventCallback> _callbacks[(int)EventsTypes::None];
+};
+
+#endif // EVENTSSERVICE_H
