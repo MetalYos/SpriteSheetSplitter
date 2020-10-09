05a60481f8647c3af659ea4a9ba69f0cd529d991
Yossi Cohen <metalyos@gmail.com>
2020-10-09 14:29:26 +0300

    Changed to Settings

--- constants.cpp
+++
@@ -1,9 +0,0 @@
-#include "constants.h"
-
-const int Constants::FRAME_INIT_WIDTH = 100;
-const int Constants::FRAME_INIT_HEIGHT = 100;
-const int Constants::FRAME_TOLERANCE_MIN = 0;
-const int Constants::FRAME_TOLERANCE_MAX = 50;
-const int Constants::FRAME_TOLERANCE_INIT = 1;
-const int Constants::FRAME_COLOR_TOLERANCE = 3;
-const char* Constants::COLOR_PICKER_CURSOR_PATH = "color_picker_cursor.png";
